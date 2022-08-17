c8e79c917cd0e96903224e7bedf196e88003604f
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-17 14:52:31 +0100

    fetch api

--- src/redux/ConfigureStore.js
+++ src/redux/ConfigureStore.js
@@ -1,4 +1,5 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import books from './books/Books';
 import categoriesReducer from './categories/Categories';
 
@@ -6,6 +7,6 @@ const rootReducer = combineReducers({
   books,
   categories: categoriesReducer,
 });
-const store = configureStore({ reducer: rootReducer });
+const store = configureStore({ reducer: rootReducer }, applyMiddleware(thunk));
 
 export default store;
--- src/redux/books/Books.js
+++ src/redux/books/Books.js
@@ -1,33 +1,16 @@
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const FETCH_BOOKS = 'bookstore/books/FETCH_BOOKS';
 
-const initialState = [
-  {
-    title: 'Invaluabe',
-    author: 'Maya Grossman',
-    id: uuidv4(),
-  },
-  {
-    title: 'Turn the ship Around',
-    author: 'L. David Marquet',
-    id: uuidv4(),
-  },
-  {
-    title: 'Dare to Lead,',
-    author: 'Brené Brown',
-    id: uuidv4(),
-  },
-  {
-    title: 'Invaluabe',
-    author: 'Maya Grossman',
-    id: uuidv4(),
-  },
-];
+const initialState = [];
 
 export default function books(state = initialState, action) {
   switch (action.type) {
+    case FETCH_BOOKS:
+      return action.books;
     case ADD_BOOK:
       return [...state, action.payload];
     case REMOVE_BOOK:
@@ -37,12 +20,37 @@ export default function books(state = initialState, action) {
   }
 }
 
-export const addBook = (book) => ({
-  type: ADD_BOOK,
-  payload: book,
-});
+export const addBook = (book) => function addBookThunk(dispatch) {
+  axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/rnyz8bgoJf9iNhz5aa7t/books', {
+    item_id: book.id,
+    title: book.title,
+    author: book.author,
+    category: 'Not Available',
+  });
+  dispatch({
+    type: ADD_BOOK,
+    book: [
+      book.id,
+      [{
+        author: book.author,
+        title: book.title,
+        category: 'Not Available',
+      }],
+    ],
+  });
+};
 
-export const removeBook = (book) => ({
-  type: REMOVE_BOOK,
-  payload: book,
-});
+export function removeBook(id) {
+  return function remoeBookThunk(dispatch) {
+    axios.delete(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/rnyz8bgoJf9iNhz5aa7t/books/${id}`);
+    dispatch({
+      type: REMOVE_BOOK,
+      id,
+    });
+  };
+}
+
+export async function fetchBooks(dispatch) {
+  const res = await axios.get('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/rnyz8bgoJf9iNhz5aa7t/books');
+  dispatch({ type: FETCH_BOOKS, books: Object.entries(res.data) });
+}
