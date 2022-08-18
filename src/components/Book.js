dc114c7810cb5db7c6db084032f5b8e1872779dd
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-18 10:55:21 +0100

    add first styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,22 +5,57 @@ import { removeBook } from '../redux/books/Books';
 
 const Book = (props) => {
   const dispath = useDispatch();
-  const { title, author, id } = props;
-  const [title2, author2, completed, currChapter] = title.split('>');
+  const {
+    title, author, id, category,
+  } = props;
+  const [title2, completed, currChapter] = title.split('>');
 
   const removeHandler = () => {
     dispath(removeBook(id));
   };
 
-
   return (
     <>
-      <ul>
-        <li>{title}</li>
-        <li>{author}</li>
-        <button type="button" onClick={removeHandler}>Remove</button>
-      </ul>
-
+      <div className="book">
+        <div className="book-desc">
+          <div className="book-obj">
+            <p className="category">{category}</p>
+            <h1 className="title">{title2}</h1>
+            <small className="author">{author}</small>
+          </div>
+          <div className="obj-actions">
+            <button type="button" className="obj-btn">Comment</button>
+            |
+            <button type="button" className="obj-btn" onClick={removeHandler}>
+              Remove
+            </button>
+            |
+            <button type="button" className="obj-btn">Edit</button>
+          </div>
+        </div>
+        <div className="progress-chapter">
+          <div className="progress">
+            <svg className="svg" width="120" height="150">
+              <circle className="circle-back" cx="50" cy="80" r="40" />
+              <circle className="circle-front" cx="50" cy="80" r="40" />
+            </svg>
+            <div>
+              <div className="percent">{completed}</div>
+              <div className="completed">completed</div>
+            </div>
+          </div>
+          <div className="pipe" />
+          <div>
+            <div className="current-chapter">Current Chapter</div>
+            <div className="chapter">
+              <span className="capterXOfY">{`Chapter ${currChapter}`}</span>
+            </div>
+            <button className="update-button" type="button">
+              Update Progress
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
@@ -28,7 +63,8 @@ const Book = (props) => {
 Book.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
-  id: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  category: PropTypes.string.isRequired,
 };
 
 export default Book;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/ConfigureStore';
 import { fetchBooks } from './redux/books/Books';
+import './style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
